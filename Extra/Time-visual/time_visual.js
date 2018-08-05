2e27de33f77b4711b91f320005eeeb3734b9c39f
Voovs <Voovs.13@gmail.com>
2018-08-05 11:59:46 -0600

    Quark_01232
    JS is clean and works *unchecked* for source_low images. Created new Typewriter folder for later Extra project

--- Extra/Time-visual/time_visual.js
+++ Extra/Time-visual/time_visual.js
@@ -1,87 +1,30 @@
-
-var now = getTime()
-
-var getTime = function(){
-  var now = converter(moment())
-  return now
-}
-
-var i = 0
-
-
-//                                                                                !!!!!!!!!! Place in initiation script to start off program
-
-//var refreshTime = function(){
-//  setInterval(function(){var now = moment(); document.getElementById("clock").innerHTML = now}, 1000)
-//}
-/*
-currTime = moment().format("hh:mm:ss a")
-trial = moment().startOf("day")
-one = moment().format("LTS")
-secs = now.seconds()
-mins = now.minutes()
-hours = now.hours()
-another = now.subtract((59/3), "hours").seconds()
-day = now.days()
-ms = now.millisecond()
-setTime = moment({hours: 5, minutes: 15, seconds: 50, miliseconds: 43})
-
-//document.getElementById("clock").innerHTML = hours*/
-
-
+/*\ TODO:
+|*| Remove intallationRun from a function
+|*| Check if setInterval() will work with being in a function
+|*| Add a check for what quality the user wants (Probably just copy paste into 3 nested functions)
+\*/
 
 var converter = function(Rnow){
   var hours = Rnow.hours() * 3.6e3
   var mins = Rnow.minutes() * 60
   var secs = Rnow.seconds()
-  return sumUp(secs, mins, hours)
-}
-
-function sumUp(){
-  var i
-  var sum = 0
-  for (i = 0; i < arguments.length; i++){
-    sum += arguments[i]
-  }
+  var sum = hours + mins + secs
   return sum
 }
-
-/*var findDifference = function(large, small){
-  var large = converter(getTime())
-  return large - converter(small)
-}*/
-/*var millisecondTime = converter(now) - converter(moment({hours: 20, minutes: 15}))
-
-if (Math.sign(millisecondTime) == -1){
-  millisecondTime = false
-  document.getElementById("clock").innerHTML = millisecondTime
-  }else{
-  document.getElementById("clock").innerHTML = millisecondTime
+//Takes in time and returns the number of seconds present
+var getTime = function(){
+  var now = converter(moment())
+  return now
 }
+//Recieves and convertes current time into seconds
 
-
-var renderCheck = function() {
-  for (var i = 0; i < 96; i++) {
-    var imageTime = converter(image[i].time)
-    var now = converter(moment())
-    var image = image[i].source_low
-
-    if (now < imageTime){
-      continue
-    }else if (now == imageTime + 90000){
-      return document.getElementById("Example").style.backgroundImage = image[i]
-    }
-    var opacitySet = (now - imageTime)/90000
-
-  }
-}*/
-//refreshTime()
-/*
-        images[x].time reffers to initial initiation - where image is at 0% opacity
-        Add 900 000 milliseconds to find final render at 100% opacity
-        Anything (18e5> i >= 900 000) will have the image stay at 100% opacity, though in the z-index: -6 <img>
-        imgTime matches indicies with image, though has the time function as the only property value and already parsed through converter()
-*/
+var initiationRun = function(){
+  var now = getTime()
+  var i = renderboot()
+  opacitySet()
+}
+initiationRun() // Function is unnecessary though assists legibility
+//Initiation boot to find starting images and opacity settings
 
 
 var image = {
@@ -205,6 +148,7 @@ var image = {
   94: {time: moment({hours: 23, minutes: 30}), source_low: "/images/time_vis/low/", source_high: "/images/time_vis/high/", source_raw: "/images/time_vis/raw/"},
   95: {time: moment({hours: 23, minutes: 45}), source_low: "/images/time_vis/low/", source_high: "/images/time_vis/high/", source_raw: "/images/time_vis/raw/"},
 }
+//Has the time and sources for each quarter
 var imgTime = {
   0: 0,
   1: 900,
@@ -301,16 +245,18 @@ var imgTime = {
   92: 82800,
   93: 83700,
   94: 84600,
-  95: 85500}   //Seconds of indicies found for image[i].time
+  95: 85500}
+// imgTime[i] === converter(image[i].time)
+
 
 var renderboot = function(){
-  var r = 0, i = 0, reference
+  var i = 0
 
   if (now === 0){    //For the midnight switch dillemma
     document.getElementById("curr_back").src = image[0].source_low
     document.getElementById("prev_back").src = image[95].source_low
     document.getElementById("curr_back").style.opacity = 0
-    return
+    return i
   }
 
     while (true){                       //Gather reference time for current foreground
@@ -322,104 +268,24 @@ var renderboot = function(){
     }
     document.getElementById("curr_back").src = image[i].source_low
     document.getElementById("prev_back").src = image[i-1].source_low
+    return i
 }
-//Activates every 15 mins- Changes curr_back and prev_back image sources          !!!!!!!!!! Must get variable "now"
+//Activates every 15 mins- Changes curr_back and prev_back image sources
 
 var opacitySet = function(){
   var setOpacity = (now - imgTime[i])/900
   document.getElementById("curr_back").style.opacity = setOpacity
 }
-//Runs every second- calculates and changes the opacity of curr_back              !!!!!!!!!! Must have access to "now" and "i"
+//Runs every second- calculates and changes the opacity of curr_back
 
 var run = function(){
   var now = getTime()
   if (now % 900 === 0){
-    renderboot()
+    i = renderboot()
   }
   opacitySet()
 }
-//Initiates above functions
+//Initiates above functions        Note: 15 mins === 900 secs
 
-var runRun = function(){
-  setInterval(run, 1000)
-}
+setInterval(run, 1000) // !!!!!!!!!! Might not work without being in function form
 //Sets an interval to run all the functions
-
-// Note that 15 mins == 900 secs
-
-
-
-
-
-
-
-
-
-
-/*
-var opacitySet = function(){
-  var imgTime = converter(image[52].time)
-  var now = converter(moment())
-
-  if (now < imgTime){
-
-  }
-
-  if (imgTime < now < (imgTime + 9e5)){
-    var setOpacity = (now - imgTime)/9e5
-    document.getElementById("curr_back").style.opacity = setOpacity
-  }
-
-  if (now < imgTime){
-    return "Image was shown before"
-  } else if(now > (imgTime + 9e5)){
-    return "Image will be shown later"
-  }else if (imgTime < now < (imgTime + 9e5)){
-    var setOpacity = (now - imgTime)/9e5
-    document.getElementById("curr_back").style.opacity = setOpacity
-    return "Should have worked"
-  }else{
-    return "Error, no conditions were matched"
-  }
-
-}
-*/
-/*
-4- Is in transition stage             if (imgTime <= now < (imgTime + 9e5))
-3- Is static background image         if ((imgTime + 9e5) <= now < (imgTime + (9e5 * 2)))
-2- Remove from static background      if (now == (imgTime + (9e5 * 2)))
-
-1- Already rendered                   if (now < imgTime)
-- Not yet in render queue            if ((imgTime + (9e5 * 2)) < now) ..Should not be necessary
-
-*/
-/*
-var experimental = function(){
-
-var r = -1
-var setOpacity
-var now = converter(getTime())*/
-/*
-while (i <= 1){
-  r += 1
-
-  if (now < imgTime[r]){ // Prior to initial render
-      continue
-  }else if ((imgTime[r] + (9e5 * 2)) <= now){ //Half an hour after initial render
-      //Add current foreground into the back
-      document.getElementById("prev_back").src = image[r + 1].source_low
-      i += 1 //Background dealt with
-  }else if ((imgTime[r] <= now < (imgTime[r] + 9e5)) and (document.getElementById("curr_back").src != image[r].source_low)){ //Checking if correct image is in curr_back
-      //true means the incorrect image is in the curr_back
-      document.getElementById("curr_back").src = image[r].source_low
-  }else if (imgTime[r] <= now < (imgTime[r] + 9e5)){ // change opacity based on difference / 9e5
-      setOpacity = (now - imgTime[r])/9e5
-      document.getElementById("curr_back").style.opacity = setOpacity
-      i += 1 //Foreground opacity correctly set
-  }else if (r > 96){
-    return "Error: all possible images have been checked"
-  }else{
-    return "Error: image time is not functional"
-  }
-}
-}*/
